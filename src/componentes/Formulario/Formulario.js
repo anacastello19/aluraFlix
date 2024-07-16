d11b2dd1beb48b8b6ecd89adb1dfabdf0e9d0a49
Ana Castello <ana99.castello@gmail.com>
2024-07-16 12:38:10 -0300

    Style form

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,12 +4,12 @@ import CampoTexto from '../CampoTexto/CampoTexto'
 const Formulario =() =>{
     return<section className='form'>
             <form>
-            <h2>Editar Card:</h2>
-            <CampoTexto title='Titulo' placeholder='Ingresar Titulo'/>    
-            <CampoTexto title='Descripcion' placeholder='Ingresar Descripcion'/> 
-            <CampoTexto title='Imagen' placeholder='Ingresar Imagen'/>   
-            <CampoTexto title='Demo' placeholder='Ingresar Imagen'/>   
-            <CampoTexto title='Repositorio' placeholder='Ingresar Repositorio'/>   
+                <h2>Editar Card:</h2>
+                <CampoTexto title='Titulo' placeholder='Ingresar Titulo'/>    
+                <CampoTexto title='Descripcion' placeholder='Ingresar Descripcion'/> 
+                <CampoTexto title='Imagen' placeholder='Ingresar Imagen'/>   
+                <CampoTexto title='Demo' placeholder='Ingresar Imagen'/>   
+                <CampoTexto title='Repositorio' placeholder='Ingresar Repositorio'/>   
             </form>
         </section>
 
